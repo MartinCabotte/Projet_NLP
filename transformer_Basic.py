b296d7faaa7a6689eb9b271697c4c4459a296e77
Leo Chartrand <leo.chartrand@usherbrooke.ca>
2023-03-16 23:57:01 -0400

    transformer fonctionnel

--- transformer_Basic.py
+++ transformer_Basic.py
@@ -11,9 +11,11 @@ from torchtext.data.utils import get_tokenizer
 from torchtext.vocab import build_vocab_from_iterator
 import torch.nn.functional as F
 from tqdm import tqdm
+import math
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print('Using {} device'.format(device))
+# device = 'cpu'
 
 class iSarcasmDataset(Dataset):
   def __init__(self, sentence, labels):
@@ -40,9 +42,9 @@ test_name = "task_A_En_test.csv"
 
 
 #On défini les paramètres du dataloader
-batch_size = 2
+batch_size = 20
 params = {'batch_size': batch_size,
-          'shuffle': False,
+          'shuffle': True,
           'num_workers': 1}
 
 
@@ -68,11 +70,27 @@ for phrase in X_train:
 X_train = X_train.drop(indsNan)
 y_train = y_train.drop(indsNan)
 
+# https://stackoverflow.com/questions/67312321/how-to-remove-urls-between-texts-in-pandas-dataframe-rows
+X_train = X_train.str.replace(r' https:\/\/.*', ' ', regex=True).str.strip()
+
 cpt = 0
 for phrase in X_train:
     if pd.isna(phrase):
         print("ERROR - il y a encore des NAN dans le dataset")
     cpt += 1
+
+# Prétraitement de X_train
+tokenizer = get_tokenizer("basic_english")
+tokenized_sequences = [tokenizer(doc) for doc in list(X_train)]
+max_sequence_length = max([len(i) for i in tokenized_sequences]) # dimension maximale des sequences
+voc = build_vocab_from_iterator(tokenized_sequences)
+indexed_sequences = [torch.tensor(voc.forward(sequence)) for sequence in tokenized_sequences]
+X_train = [torch.cat([sequence,torch.tensor([voc.__len__()]).expand(max_sequence_length- len(sequence))]) for sequence in indexed_sequences]
+y_train = torch.tensor(y_train.values).unsqueeze(1).float()
+
+print(type(X_train))
+print(type(y_train))
+
 
 #On créé notre dataset pyTorch
 train_iSarcasm = iSarcasmDataset(X_train, y_train)
@@ -80,10 +98,10 @@ training_generator = DataLoader(train_iSarcasm, **params)
 
 
 #On regarde que tout soit bien chargé
-for i, (batch, labels) in enumerate(training_generator):
+for i, (seq, labels) in enumerate(training_generator):
     if i > 0:
         break
-    print("Batch size : "+str(len(batch)))
+    print("Batch size : "+str(len(seq)))
     
 #On va créer nos données de validation
 panda_Test = pd.read_csv(directory+test_name, index_col=[0])
@@ -95,56 +113,91 @@ test_iSarcasm = iSarcasmDataset(X_test, y_test)
 test_generator = DataLoader(test_iSarcasm, **params)
 
 #On regarde que tout soit bien chargé
-for i, (batch, labels) in enumerate(test_generator):
+for i, (seq, labels) in enumerate(test_generator):
     if i > 0:
         break
-    print("Batch size : "+str(len(batch)))
+    print("Batch size : "+str(len(seq)))
     
 print("--- Chargement des datasets terminé ---")
 
 
 # On créé le vocabulaire
 
-print("--- Traitement sur notre vocabulaire ---")
+# print("--- Traitement sur notre vocabulaire ---")
 
-list_of_words = []
-for phrase in X_train:
-    list_of_words.extend(phrase.split())
+# list_of_words = []
+# for phrase in X_train:
+#     list_of_words.extend(phrase.split())
     
-list_of_words = np.unique(list_of_words)
-len_Vocabulary = len(list_of_words)
+# list_of_words = np.unique(list_of_words)
+# len_Vocabulary = len(list_of_words)
 
-print("La longueur du vocabulaire est de : "+str(len_Vocabulary))
+# print("La longueur du vocabulaire est de : "+str(len_Vocabulary))
 
 print("--- Création du Transformer basique ---")
 
+
+"""
+La classe PositionalEncoding est offerte par le tutoriel officiel de PyTorch.
+https://pytorch.org/tutorials/beginner/transformer_tutorial.html
+torch.nn n'implemente pas cette classe a ce jour.
+"""
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model, vocab_size=5000, dropout=0.1):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(vocab_size, d_model)
+        position = torch.arange(0, vocab_size, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(
+            torch.arange(0, d_model, 2).float()
+            * (-math.log(10000.0) / d_model)
+        )
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)
+        self.register_buffer("pe", pe)
+
+    def forward(self, x):
+        x = x + self.pe[:, : x.size(1), :]
+        return self.dropout(x)
+
 class Transformer(nn.Module):
-    def __init__(self, len_vocab, len_embedding, nhead, num_layers):
+    def __init__(self, len_vocab, embed_size, nhead, num_layers):
         super(Transformer, self).__init__()
+
+        # 
+        self.len_embedding = embed_size
+
+        # Masque pour le transformeur
+        self.mask = nn.Transformer.generate_square_subsequent_mask(max_sequence_length).to(device)
         
-        #On créé la layer de l'embedding
-        self.embedding = nn.Embedding(len_vocab, len_embedding)
+        # On créé la layer de l'embedding
+        self.embedding = nn.Embedding(len_vocab+1, embed_size, padding_idx=len_vocab)
 
-        #Le transformer
-        self.transformer = nn.Transformer(len_embedding, nhead, num_layers)
+        # Positional encoding 
+        self.positional_encoder = PositionalEncoding(embed_size,vocab_size=len_vocab)
 
-        #La layer fully connected pour déterminer si la phrase est sarcastique ou non
-        self.fc = nn.Linear(len_embedding, 2)
+        # Le transformer
+        self.transformer_encoder = nn.TransformerEncoder(nn.TransformerEncoderLayer(embed_size, nhead), num_layers)
+        # self.transformer = nn.Transformer(len_embedding, nhead, num_layers)
+
+        # La layer fully connected pour déterminer si la phrase est sarcastique ou non
+        self.fc = nn.Linear(embed_size, 1)
+
 
     def forward(self, x, label):
         
         #Pass avant de notre modèle
-        x = self.embedding(x)
-        x = self.transformer(x, label)
+        x = self.embedding(x) * math.sqrt(self.len_embedding)
+        x = self.positional_encoder(x)
+        x = self.transformer_encoder(x)
+        x = x.mean(dim=0)
         x = self.fc(x)
         return x
     
 print("--- Création du Transformer basique terminée ---")
-
-tokenizer = get_tokenizer("basic_english")
-tokens = [tokenizer(doc) for doc in list(X_train)]
-
-voc = build_vocab_from_iterator(tokens)
 
 def one_hot_encoding(voc, list_Of_Words):
     indices = voc.forward(list_Of_Words)
@@ -153,20 +206,22 @@ def one_hot_encoding(voc, list_Of_Words):
     return matrixToReturn
 
 # Paramètres de notre modèle
-taille_embeddings = 10
+taille_embeddings = 100
 nhead = 2
 num_layers = 3
-n_epoch = 1
+n_epoch = 10
 
 
 # On va optimiser notre modèle
 import torch.optim as optim
 
 # On déclare notre modèle
-model = Transformer(voc.__len__(),taille_embeddings,nhead,num_layers)
+model = Transformer(voc.__len__(),taille_embeddings,nhead,num_layers).to(device)
+# print(voc.__len__())
+# print(len_Vocabulary)
 
 # On déclare notre loss
-loss = nn.CrossEntropyLoss()
+criterion = nn.BCEWithLogitsLoss()
 
 # On se déclare un optimiseur qui effectuera la descente de gradient
 optimizer = optim.Adam(model.parameters())
@@ -179,35 +234,26 @@ for epoch in range(n_epoch):
     
     running_loss = 0
     # On loop sur le batch
-    for i, (batch, labels) in enumerate(training_generator):
+    for i, (seq, labels) in enumerate(training_generator):
         
-        tokens = [tokenizer(doc) for doc in list(batch)]
-        one_hot_matrix = np.array([])
-
-        for number_batch in range(batch_size):
-            np.append(one_hot_matrix,one_hot_encoding(voc,tokens[number_batch]))
-        
-        print(one_hot_matrix.shape)
-        one_hot_matrix = torch.tensor(one_hot_matrix).to(torch.int64).to(device)
-        labels = labels.to(device)
+        sequences = torch.transpose(torch.cat([sequence[None] for sequence in  list(seq)]),0,1).to(torch.int64).to(device)
+        labels = labels.to(device)  
 
         # zero the parameter gradients
         optimizer.zero_grad()
     
         # forward + backward + optimize
-        outputs = model(one_hot_matrix,labels)
+        outputs = model(sequences,labels)
+        # print(outputs)
 
-        print("Après")
-        loss = loss(outputs, labels)
-        print("ok")
+        loss = criterion(outputs, labels)
         loss.backward()
-        print("ok")
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
         optimizer.step()
-        print("ok")
     
         # print statistics
         running_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
+        if i % 20 == 19:    # print every 2000 mini-batches
             print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
             running_loss = 0.0                                                      
             
